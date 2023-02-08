1edbee5c1f1a01f4315cd9ec6888ef4f8045012e
mohamedelbadawi <redamohamed619@gmail.com>
2023-02-08 20:06:32 +0200

    apple iphone landing page

---
+++ Apple/main.js
@@ -0,0 +1,8 @@
+let img = document.querySelector('.img')
+let container = document.querySelector('.container')
+function updatePhones(phone) {
+    img.src = phone
+}
+function updateColors(color) {
+    container.style.background = color;
+}
